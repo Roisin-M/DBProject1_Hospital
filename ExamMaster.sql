df49cfe53e6174ba503555b1bab466bf589744f3
Roisin-M <s00223865@atu.ie>
2025-03-04 13:20:00 +0000

    DRAFT MASTER SPROC READS FOR WARD CAPACITY RULE

--- ExamMaster.sql
+++ ExamMaster.sql
@@ -5,12 +5,20 @@ CREATE PROC ExamMaster
 , @ECareTeamIDS PCareTeams READONLY
 AS
 --declare all internal variables
-
-
---read data from the tables/system data in this section
---into the internal variables
+DECLARE 
+@INumOfPatientsInTheWard INT, @IPatientsAge INT, 
+@ITodaysDate DATE = GETDATE(), @IDayOfWeek INT --sunday =1, saturday =7
+--READ DATA FROM THE TABLES/ SYSTEM INTO THE INTERNAL VARIABLES
+--READ DAY OF THE WEEK
+SELECT @IDayOfWeek = DATENAME(DW,@ITodaysDate)
+-- READ NUMBER OF PATIENTS IN THE WARD
+SELECT @INumOfPatientsInTheWard = COUNT(PatientWarD)
+FROM dbo.PatientTBL 
+WHERE @EWardID = PatientWarD
 
 --perform all logic operations here
+--IS IT A WEEKDAY
+IF  
 
 --execute all output sub procs here
 --wrap each execute in a TRY/CATCH block
