4985fb5cc9f25e4ff67204dea7d59b44cb05d12b
Roisin-M <s00223865@atu.ie>
2025-03-04 14:52:58 +0000

    WARD CAPACITY BREAACH READS AND LOGIC COMPLETED

--- ExamMaster.sql
+++ ExamMaster.sql
@@ -6,8 +6,9 @@ CREATE PROC ExamMaster
 AS
 --declare all internal variables
 DECLARE 
-@INumOfPatientsInTheWard INT, @IPatientsAge INT, 
+@INumOfPatientsInTheWard TINYINT, @IPatientsAge INT, 
 @ITodaysDate DATE = GETDATE(), @IDayOfWeek INT --sunday =1, saturday =7
+, @IWardCapacity TINYINT
 --READ DATA FROM THE TABLES/ SYSTEM INTO THE INTERNAL VARIABLES
 --READ DAY OF THE WEEK
 SELECT @IDayOfWeek = DATENAME(DW,@ITodaysDate)
@@ -15,10 +16,29 @@ SELECT @IDayOfWeek = DATENAME(DW,@ITodaysDate)
 SELECT @INumOfPatientsInTheWard = COUNT(PatientWarD)
 FROM dbo.PatientTBL 
 WHERE @EWardID = PatientWarD
+-- READ WARD CAPACITY
+SELECT @IWardCapacity = WardCapacity
+FROM DBO.WarDTBL
+WHERE @EWardID = WardID
 
---perform all logic operations here
+--PERFORM ALL BUSINESS LOGIC OPERATIONS HERE
 --IS IT A WEEKDAY
-IF  
+IF  (@IDayOfWeek = 1 OR @IDayOfWeek = 7)
+BEGIN
+    -- IS THERE A WARD CAPACITY BREACH
+    IF @INumOfPatientsInTheWard >= ((@IWardCapacity * 1.2)-1)
+    BEGIN
+    ;THROW 50001, 'The Ward Capacity Is Overflowing', 1
+    END
+END
+ELSE 
+BEGIN
+    -- IS THERE A WARD CAPACITY BREACH
+    IF @INumOfPatientsInTheWard >= (@IWardCapacity - 1)
+    BEGIN
+    ;THROW 50002, 'The Ward Capacity Is Full', 1
+    END
+END
 
 --execute all output sub procs here
 --wrap each execute in a TRY/CATCH block
