0c4a7b9b692bbb2dae280214797f277073b9e32f
Roisin-M <s00223865@atu.ie>
2025-03-04 11:50:24 +0000

    insert patient to care team query

---
+++ InsertPatientToCareTeam.sql
@@ -0,0 +1,18 @@
+CREATE PROC InsertPatient_ToCareTeam
+@EpPatientID INT,
+@EcDateFormed DATE,
+@EcDateFinished DATE,
+@OCareTeamID INT OUTPUT
+AS
+--insert values into Care Team Table
+BEGIN TRY
+INSERT into dbo.CareTeamTBL
+(PatientID, DateFormed, DateFinished)
+VALUES
+(@EpPatientID, @EcDateFormed, @EcDateFinished)
+-- output the newly generated careteam ID
+SELECT @OCareTeamID = SCOPE_IDENTITY()
+END TRY
+BEGIN CATCH
+;THROW
+END CATCH
