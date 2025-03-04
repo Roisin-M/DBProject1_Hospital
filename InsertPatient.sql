156775b1daaba9c6c933028205d9a3c4b1e13434
Roisin-M <s00223865@atu.ie>
2025-03-04 11:44:22 +0000

    Insert Patient

---
+++ InsertPatient.sql
@@ -0,0 +1,19 @@
+CREATE PROC InsertPatient
+@EpFirstName VARCHAR(35),
+@EpLastName VARCHAR(35),
+@EpWardID INT,
+@EpCovidStatus CHAR(8),
+@OpatientNum INT OUTPUT
+AS
+--insert values into Patient TBL
+BEGIN TRY
+INSERT into dbo.PatientTBL
+(PatientFname, PatientLname, PatientWarD, PatientCOVIDStatus)
+VALUES
+(@EpFirstName, @EpLastName, @EpWardID, @EpCovidStatus)
+-- output the newly generated patient ID
+SELECT @OpatientNum = SCOPE_IDENTITY()
+END TRY
+BEGIN CATCH
+;THROW
+END CATCH
