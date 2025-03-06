c9310f17304cabd86cf744fd5130a26ff8fd3919
Roisin-M <s00223865@atu.ie>
2025-03-06 19:39:29 +0000

    CARE TEAM RULES COMPLETE EXCEPT FOR SELECTINGNURSE

--- ExamMaster.sql
+++ ExamMaster.sql
@@ -1,19 +1,23 @@
 CREATE PROC ExamMaster
 --***DECLARE ALL EXTERNAL DATA***
 @EFirstName VARCHAR(35), @ELastName VARCHAR(35), @EDOB DATE, @ECovidStatus BIT, @EWardID INT
+, @ECareTeamID INT
 --TABLE TYPE CARE TEAMIDS
-, @ECareTeamIDS PCareTeams READONLY
+  --@ECareTeamIDS PCareTeams READONLY
 AS
 --***DECLARE ALL INTERNAL VARIABLES***
 DECLARE 
-@INumOfPatientsInTheWard TINYINT, @IPatientsAge INT, 
+@INumOfPatientsInTheWard TINYINT, @IPatientsAge TINYINT, 
 @ITodaysDate DATE = GETDATE(), @IDayOfWeek INT --sunday =1, saturday =7
 , @IWardCapacity TINYINT, @IWardStatus CHAR(10), @IWardSpec CHAR(10)
+, @INumberOfDoctorsInCareTeam TINYINT, @INumberOfNursesInCareTeam TINYINT,
+@INumberOfDoctorSpecialityMatches TINYINT, @INumberOfNursesSpecialityMatches TINYINT
+, @INumberOfVaccinatedDoctors TINYINT, @INumberOfVaccinatedNurses TINYINT
 --***READ DATA FROM THE TABLES/ SYSTEM INTO THE INTERNAL VARIABLES***
 --READ DAY OF THE WEEK
 SELECT @IDayOfWeek = DATENAME(WEEKDAY,@ITodaysDate)
 -- READ NUMBER OF PATIENTS IN THE WARD
-SELECT @INumOfPatientsInTheWard = COUNT(PatientWarD)
+SELECT @INumOfPatientsInTheWard = COUNT(*)
 FROM dbo.PatientTBL 
 WHERE @EWardID = PatientWarD
 -- READ WARD CAPACITY
@@ -26,6 +30,44 @@ SELECT @IPatientsAge = DATEDIFF(YEAR,@EDOB,@ITodaysDate)
 SELECT @IWardSpec = WardSpeciality
 FROM dbo.WarDTBL
 WHERE @EWardID = WardID
+-- READ NUMBER OF DOCTORS IN CARE TEAM ID
+SELECT @INumberOfDoctorsInCareTeam = COUNT(*)
+FROM DBO.DoctorCareTeamMembersTBL
+WHERE @ECareTeamID = CareTeamID
+AND CurrentMember != 0
+-- READ NUMBER OF NURSES IN CARE TEAM ID
+SELECT @INumberOfNursesInCareTeam = COUNT(*)
+FROM DBO.NurseCareTeamMembersTBL
+WHERE @ECareTeamID = CareTeamID
+AND CurrentMember !=0
+-- READ NUMBER OF VACCINATED DOCTORS IN CARE TEAM
+SELECT @INumberOfVaccinatedDoctors = COUNT(*)
+FROM DBO.DoctorTBL D
+JOIN DBO.DoctorCareTeamMembersTBL DC ON D.DoctorID = DC.MemberID
+WHERE @ECareTeamID = DC.CareTeamID
+AND DC.CurrentMember != 0
+AND D.COVID19Vacinated = 1; 
+-- READ NUMBER OF VACCINATED NURSES IN CARE TEAM
+SELECT @INumberOfVaccinatedNurses = COUNT(*)
+FROM DBO.NurseTBL N
+JOIN DBO.NurseCareTeamMembersTBL NC ON N.NurseID = NC.MemberID
+WHERE @ECareTeamID = NC.CareTeamID
+AND NC.CurrentMember != 0
+AND N.COVID19Vacinated = 1; 
+-- READ NUMBER OF DOCTORS WHOS SPECIALITY MATCHES THAT OF THE WARD
+SELECT @INumberOfDoctorSpecialityMatches = COUNT(*)
+FROM dbo.DoctorTBL D
+JOIN dbo.DoctorCareTeamMembersTBL DC ON D.DoctorID = DC.MemberID
+WHERE @ECareTeamID = DC.CareTeamID
+AND DC.CurrentMember != 0
+AND LEFT(@IWardSpec, 3) = RIGHT(D.DoctorSpeciality, 3);
+-- READ NUMBER OF NURSES WHOS SPECIALITY MATCHES THAT OF THE WARD
+SELECT @INumberOfNursesSpecialityMatches = COUNT(*)
+FROM dbo.NurseTBL N
+JOIN dbo.NurseCareTeamMembersTBL NC ON N.NurseID = nc.MemberID
+WHERE @ECareTeamID = NC.CareTeamID
+AND NC.CurrentMember != 0
+AND LEFT(@IWardSpec, 3) = RIGHT(N.NurseSpeciality, 3);
 --***PERFORM ALL BUSINESS LOGIC OPERATIONS HERE***
 --1. WARD CAPACITY RULES
 -- DEFAULT VALUE FOR WARD STATUS
@@ -51,21 +93,64 @@ BEGIN
 END
 --2. WARD AGE RULES
 --CHECK MATCHES PAEDS 13
-IF (@IPatientsAge <=13 AND (@IWardSpec NOT LIKE '%Paediatric13%' OR @IWardSpec NOT LIKE '%Paeds 13%'))
-    BEGIN
-    ;THROW 50003, 'Patient is less than 13 but not assigned to "Paediatric13" or "Paeds 13"',1
-    END
+IF (@IPatientsAge <=13 AND (@IWardSpec NOT LIKE 'Paediatric13' OR @IWardSpec NOT LIKE 'Paeds 13'))
+BEGIN
+;THROW 50003, 'Patient is less than 13 but not assigned to "Paediatric13" or "Paeds 13"',1
+END
 --CHECK MATCHES PAEDS15
-ELSE IF (@IPatientsAge = 14 AND (@IWardSpec NOT LIKE '%Paediatric15%' OR @IWardSpec NOT LIKE '%Paeds 15%'))
-    BEGIN
-    ;THROW 50004, 'Patient is greater than 13and less than 15 but not assigned to "Paediatric15" or "Paeds 15"',1
-    END
+ELSE IF (@IPatientsAge = 14 AND (@IWardSpec NOT LIKE 'Paediatric15' OR @IWardSpec NOT LIKE 'Paeds 15'))
+BEGIN
+;THROW 50004, 'Patient is greater than 13and less than 15 but not assigned to "Paediatric15" or "Paeds 15"',1
+END
 --CHECK MATCHES PAEDS
-ELSE IF (@IPatientsAge >=15 AND @IPatientsAge <18 AND (@IWardSpec NOT LIKE '%Paediatric%' OR @IWardSpec NOT LIKE '%Paeds%'))
+ELSE IF (@IPatientsAge >=15 AND @IPatientsAge <18 AND (@IWardSpec NOT LIKE 'Paediatric' OR @IWardSpec NOT LIKE 'Paeds'))
+BEGIN
+;THROW 50005, 'Patient is greater than 14 and less than 18 but not assigned to "Paediatric" or "Paeds"',1
+END
+--3. CARE-TEAM RULES
+--CHECK IF COVID STATUS IS POSITIVE /UNKNOWN
+ELSE IF(@ECovidStatus LIKE 'positive' OR @ECovidStatus LIKE 'unknown')
+BEGIN
+    --CHECK IF ALL DOCTORS ARE VACCINATED IN THE CARE TEAM
+    IF(@INumberOfVaccinatedDoctors < @INumberOfDoctorsInCareTeam)
     BEGIN
-    ;THROW 50005, 'Patient is greater than 14 and less than 18 but not assigned to "Paediatric" or "Paeds"',1
+    ;THROW 50009, 'Patient covid status is positive or unknown and not all the doctors in the care team are vaccinated', 1;
     END
-
+    --CHECK IF ALL NURSES ARE VACCINATED IN THE CARE TEAM
+    ELSE IF(@INumberOfVaccinatedNurses < @INumberOfNursesInCareTeam)
+    BEGIN
+    ;THROW 50010, 'Patient covid status is positive or unknown and not all the nurses in the care team are vaccinated', 1;
+    END
+END
+--CHECK IF THERE IS AT LEAST 1 DOCTOR AND AT LEAST 2 NURSES
+ELSE IF(@INumberOfDoctorsInCareTeam = 0 AND @INumberOfNursesInCareTeam < 2)
+BEGIN
+    --CHECK IF THERE IS AT LEAST 1 DOCTOR 
+    IF(@INumberOfDoctorsInCareTeam = 0 )
+    BEGIN
+    ;THROW 50006, 'care Team Does Not Have At Least 1 Active Doctor',1
+    END
+    -- CHECK IF THERE IS AT LEAST 1 NURSE
+    ELSE IF(@INumberOfNursesInCareTeam = 0 )
+    BEGIN
+    ;THROW 50006, 'care Team Does Not Have At Least 1 Active Nurse',1
+    END
+    -- CHECK IF THERE IS AN AVAILABLE NURSE
+    ELSE
+    BEGIN
+    -- INSERT NURSE INTO CARETEAM
+    END
+END
+-- CHECK THAT AT LEAST 1 DOCTOR HAS SPECIALITY OF WARD
+IF(@INumberOfDoctorSpecialityMatches = 0)
+BEGIN
+;THROW 50007, 'Care Team Does Not Have At Least One Active Doctor With The Ward Speciality', 1;
+END
+-- CHECK THAT AT LEAST 1 NURSE HAS SPECIALITY OF WARD
+ELSE IF (@INumberOfNursesSpecialityMatches = 0)
+BEGIN
+;THROW 50008, 'Care Team Does Not Have At Least One Active Nurse With The Ward Speciality', 1;
+END
 --***EXECUTE SUBSPROCS***
 --wrap each execute in a TRY/CATCH block
 
